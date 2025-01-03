9fa6dae7d9344e7e77281a294b4390c9e66572e1
Shawn Hsu <shawn009@gpu02.local.local>
2025-01-03 14:05:31 +0800

    feat: add model comparision visualizer

--- ltt_ff_frontend/run_defect_ui.py
+++ ltt_ff_frontend/run_defect_ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from loguru import logger
 
-from ltt_ff_frontend.defect_ui import defect_viz, inference_api
+from ltt_ff_frontend.defect_ui import defect_viz, inference_api, defect_dshbrd
 
 
 if __name__ == "__main__":
@@ -11,8 +11,10 @@ if __name__ == "__main__":
                          title="Defect visualization", icon=":material/browse_activity:")
     inference_api = st.Page(inference_api.app, url_path="inference_api",
                             title="Inference", icon=":material/search_check_2:")
+    defect_dshbrd = st.Page(defect_dshbrd.app, url_path="defect_dshbrd", 
+                        title="Defect Probability Visualization Dashboard", icon=":material/search_check_2:")
 
-    pg = st.navigation([defect_viz, inference_api])
+    pg = st.navigation([defect_viz, inference_api, defect_dshbrd])
 
     st.set_page_config(page_title="Lasertec Defect Filter UI", page_icon=":material/manufacturing:")
 
