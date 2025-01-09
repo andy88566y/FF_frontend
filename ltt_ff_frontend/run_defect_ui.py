ac468b9bcbd62ab29808f1fdfb0afb3ec0aa90dd
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-01-09 16:36:44 +0800

    fix: rename defect dashboard

--- ltt_ff_frontend/run_defect_ui.py
+++ ltt_ff_frontend/run_defect_ui.py
@@ -1,21 +1,21 @@
 import streamlit as st
 from loguru import logger
 
-from ltt_ff_frontend.defect_ui import defect_viz, inference_api, defect_dshbrd, finetuning_api
+from ltt_ff_frontend.defect_ui import defect_viz, defect_dashboard, inference_api, finetuning_api
 
 
 if __name__ == "__main__":
     logger.debug("Loading main UI ...")
 
-    defect_viz = st.Page(defect_viz.app, url_path="defect_viz",
-                         title="Defect visualization", icon=":material/browse_activity:")
     inference_api = st.Page(inference_api.app, url_path="inference_api",
                             title="Inference", icon=":material/search_check_2:")
     finetuning_api = st.Page(finetuning_api.app, url_path="training_api", title="Fine-tuning", icon=":material/build:")
-    defect_dshbrd = st.Page(defect_dshbrd.app, url_path="defect_dshbrd",
+    defect_dashboard = st.Page(defect_dashboard.app, url_path="defect_dashboard",
                         title="Defect Probability Visualization Dashboard", icon=":material/search_check_2:")
+    defect_viz = st.Page(defect_viz.app, url_path="defect_viz",
+                         title="Defect visualization", icon=":material/browse_activity:")
 
-    pg = st.navigation([defect_viz, inference_api, finetuning_api, defect_dshbrd])
+    pg = st.navigation([defect_dashboard, inference_api, finetuning_api, defect_viz])
 
     st.set_page_config(page_title="Lasertec Defect Filter UI", page_icon=":material/manufacturing:")
 
