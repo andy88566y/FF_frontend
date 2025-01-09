50f1e90cff36e94cf2fd3bda63d7a16ce561dea0
Shawn Hsu <shawn009@gpu02.local.local>
2025-01-09 15:58:58 +0800

    feat: modified finetune's paginating and progress bar

--- ltt_ff_frontend/defect_ui/finetuning_api.py
+++ ltt_ff_frontend/defect_ui/finetuning_api.py
@@ -3,6 +3,26 @@ import streamlit as st
 from ltt_ff_frontend.defect_ui import defect_ui_helper as helper
 
 
+def dummy_get_progress(status):
+    if status == "starting":
+        return 10
+    elif status == "running":
+        return 70
+    elif status == "completed":
+        return 100
+    elif status == "error":
+        return 0
+    else:
+        return 0
+
+def get_color(status):
+    if status in ["starting", "running", "completed"]:
+        return "green"
+    elif status == "error":
+        return "red"
+    else:
+        return "grey"
+
 def app() -> None:
     st.title("BDDetector False Filtering API")
     st.caption("API for running inference and fine-tuning False Filtering models.")
@@ -59,7 +79,7 @@ def app() -> None:
             "best_model_path",
             "message",
             "error_message"]
-        brief = ["status","estimated_time_remaining","current_epoch","total_epochs","epoch_loss",
+        brief = ["status",'progress_bar',"estimated_time_remaining","current_epoch","total_epochs","epoch_loss",
             "val_loss"]
         for status in all_statuses:
             new_row = pd.DataFrame([status])
@@ -69,27 +89,48 @@ def app() -> None:
             if detail_status_df[column].dtype == 'object':
                 detail_status_df[column] = detail_status_df[column].astype(str)
 
-
+        detail_status_df['progress_bar'] = detail_status_df['status'].apply(dummy_get_progress)
+        detail_status_df['color'] = detail_status_df['status'].apply(get_color)
         detail_status_df = detail_status_df.iloc[::-1].reset_index(drop=True)
         status_df = detail_status_df[brief]
 
         st.session_state.status_df = status_df
         st.session_state.detail = detail_status_df[reorder]
 
+    progress_column = st.column_config.ProgressColumn(
+        label="progress_bar",
+        min_value=0,
+        max_value=100
+    )
+
     if "status_df" not in st.session_state:
         st.session_state.status_df = pd.DataFrame(columns = brief)
     if "detail" not in st.session_state:
         st.session_state.detail = pd.DataFrame(columns = reorder)
 
+    # Pagination settings
+    if not st.session_state.status_df.empty:
+        page_size = 10
+        page_number = st.number_input('Page number', min_value=1, value=1, step=1)
+        start_index = (page_number - 1) * page_size
+        end_index = page_number * page_size
+
+
+    # Selection to find more detail
     event = st.dataframe(
-        st.session_state.status_df,
+        st.session_state.status_df.iloc[start_index:end_index],
         key = "statuses",
         on_select = "rerun",
         selection_mode = "multi-row",
         use_container_width=True,
-    )
+        column_config={"progress_bar": progress_column}
+    ) if not st.session_state.status_df.empty else st.write("")
+
+
     if event and event.selection:
         if event.selection["rows"]:
-            selected_rows = st.session_state.detail.iloc[event.selection["rows"]]
+            selected_indices = [start_index + st.session_state.status_df.index[i] for i in event.selection["rows"]]
+            selected_rows = st.session_state.detail.loc[selected_indices]
+
             transposed_detail = selected_rows.T
-            st.write(transposed_detail)
+            st.dataframe(transposed_detail, use_container_width= True )
