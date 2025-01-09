74d19a38b184fe198a438a2bab2f2b10297de727
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-01-09 17:00:26 +0800

    style: fix formatting

--- ltt_ff_frontend/defect_ui/defect_dashboard.py
+++ ltt_ff_frontend/defect_ui/defect_dashboard.py
@@ -1,20 +1,10 @@
-import glob
-import os
-
-import pandas as pd
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
-import random
-
-import requests
 import streamlit as st
 from loguru import logger
 
-from ltt_ff_frontend.constant import API_ROOT
 from ltt_ff_frontend.defect_ui import defect_ui_helper as helper
 
-# Modified as of 01/07
 
 def model_data(db_path: str) -> dict:
     defect_id_list = helper.get_defect_id(db_path)
@@ -32,15 +22,16 @@ def model_data(db_path: str) -> dict:
     }
     return data
 
+
 def generate_2D_plot(model_1: dict, model_2: dict, slith1: float, slith2: float):
 
     probabilities_mod1 = [entry['probability'] for entry in model_1.values()]
-    yes_prob_mod1 = [entry['probability'] for entry in model_1.values() if entry['answer'] == True]
-    no_prob_mod1 = [entry['probability'] for entry in model_1.values() if entry['answer'] == False]
+    yes_prob_mod1 = [entry['probability'] for entry in model_1.values() if entry['answer']]
+    no_prob_mod1 = [entry['probability'] for entry in model_1.values() if not entry['answer']]
 
     probabilities_mod2 = [entry['probability'] for entry in model_2.values()]
-    yes_prob_mod2 = [entry['probability'] for entry in model_2.values() if entry['answer'] == True]
-    no_prob_mod2 = [entry['probability'] for entry in model_2.values() if entry['answer'] == False]
+    yes_prob_mod2 = [entry['probability'] for entry in model_2.values() if entry['answer']]
+    no_prob_mod2 = [entry['probability'] for entry in model_2.values() if not entry['answer']]
 
     # true / false to be determined
 
@@ -51,9 +42,9 @@ def generate_2D_plot(model_1: dict, model_2: dict, slith1: float, slith2: float)
 
     indices = [entry["defect_id"] for entry in model_1.values()]
 
-    colors = ['rgba(0, 255, 0, 0.3)' if a1 == True and a2 == True else
-            'rgba(255, 0, 0, 0.3)' if a1 == False and a2 == False else
-            'blue' for a1, a2 in zip(answer_1, answer_2)]
+    colors = ['rgba(0, 255, 0, 0.3)' if a1 and a2 else
+              'rgba(255, 0, 0, 0.3)' if not a1 and not a2 else
+              'blue' for a1, a2 in zip(answer_1, answer_2)]
 
     # 2D scatter plot
     fig = go.Figure()
@@ -63,10 +54,10 @@ def generate_2D_plot(model_1: dict, model_2: dict, slith1: float, slith2: float)
             xaxis = 'x',
             yaxis = 'y',
             mode = 'markers',
-            marker = dict(
-                color = colors,
-                size = 5
-            ),
+            marker = {
+                "color": colors,
+                "size": 5,
+            },
             text=[f'Index: {index}' for index in indices],
             hoverinfo='text',
             hovertemplate='%{text}<br>Model 1 Pred: %{x}<br>Model 2 Pred: %{y}'
@@ -74,79 +65,74 @@ def generate_2D_plot(model_1: dict, model_2: dict, slith1: float, slith2: float)
 
 
     # threshold plot
-    fig.add_shape(type='line',
-              x0= threshold_1, x1=threshold_1, y0=0, y1=1,
-              xref='x', yref='paper',
-              line=dict(color='Red', width=2, dash='dash'))
+    fig.add_shape(type='line', x0= threshold_1, x1=threshold_1, y0=0, y1=1, xref='x', yref='paper',
+                  line={"color": 'Red', "width": 2, "dash": "dash"})
 
-    fig.add_shape(type='line',
-              x0= 0, x1= 1, y0=threshold_2, y1=threshold_2,
-              xref='paper', yref='y',
-              line=dict(color='Red', width=2, dash='dash'))
+    fig.add_shape(type='line', x0= 0, x1= 1, y0=threshold_2, y1=threshold_2, xref='paper', yref='y',
+                  line={"color": 'Red', "width": 2, "dash": "dash"})
 
     # histograms
     fig.add_trace(go.Histogram(
-            y = yes_prob_mod2,
-            xaxis = 'x2',
-            marker = dict(
-                color = 'olivedrab'
-            ),
-            ybins=dict(start=0.00, end=1.00, size=0.01)
-        ))
+        y = yes_prob_mod2,
+        xaxis = 'x2',
+        marker = {
+            "color": 'olivedrab'
+        },
+        ybins={"start": 0.00, "end": 1.00, "size": 0.01}
+    ))
     fig.add_trace(go.Histogram(
         y = no_prob_mod2,
         xaxis = 'x2',
-        marker = dict(
-            color = 'darkred'
-        ),
-        ybins=dict(start=0.00, end=1.00, size=0.01)
+        marker = {
+            "color": 'darkred'
+        },
+        ybins={"start": 0.00, "end": 1.00, "size": 0.01}
     ))
 
     fig.add_trace(go.Histogram(
-            x = yes_prob_mod1,
-            yaxis = 'y2',
-            marker = dict(
-                color = 'olivedrab'
-            ),
-            xbins=dict(start=0.00, end=1.00, size=0.01)
-        ))
+        x = yes_prob_mod1,
+        yaxis = 'y2',
+        marker = {
+            "color": 'olivedrab'
+        },
+        xbins={"start": 0.00, "end": 1.00, "size": 0.01}
+    ))
 
     fig.add_trace(go.Histogram(
-            x = no_prob_mod1,
-            yaxis = 'y2',
-            marker = dict(
-                color = 'darkred'
-            ),
-            xbins=dict(start=0.00, end=1.00, size=0.01)
-        ))
+        x = no_prob_mod1,
+        yaxis = 'y2',
+        marker = {
+            "color": 'darkred'
+        },
+        xbins={"start": 0.00, "end": 1.00, "size": 0.01}
+    ))
 
     fig.update_layout(
         autosize = False,
-        xaxis = dict(
-            zeroline = False,
-            domain = [0,0.85],
-            showgrid = False,
-            title = f'Model:1'
-        ),
-        yaxis = dict(
-            zeroline = False,
-            domain = [0,0.85],
-            showgrid = False,
-            title = f'Model:2'
-
-        ),
-        xaxis2 = dict(
-            zeroline = False,
-            domain = [0.85,1],
-            showgrid = False,
-            title = f'Model:1'
-        ),
-        yaxis2 = dict(
-            zeroline = False,
-            domain = [0.85,1],
-            showgrid = False,
-            title = f'Model:2'
-        ),
+        xaxis = {
+            "zeroline": False,
+            "domain": [0,0.85],
+            "showgrid": False,
+            "title": 'Model:1'
+        },
+        yaxis = {
+            "zeroline": False,
+            "domain": [0,0.85],
+            "showgrid": False,
+            "title": 'Model:2'
+        },
+        xaxis2 = {
+            "zeroline": False,
+            "domain": [0,0.85],
+            "showgrid": False,
+            "title": 'Model:1'
+        },
+        yaxis2 = {
+            "zeroline": False,
+            "domain": [0,0.85],
+            "showgrid": False,
+            "title": 'Model:2'
+        },
         height = 600,
         width = 600,
         bargap = 0,
@@ -157,45 +143,39 @@ def generate_2D_plot(model_1: dict, model_2: dict, slith1: float, slith2: float)
 
     return fig
 
+
 def generate_1D_plot(model: dict, slith: float):
-
-    probabilities = [entry['probability'] for entry in model.values()]
-    indices = [entry["defect_id"] for entry in model.values()]
-
-    # to be determined
-    true_probs = [entry['probability'] for entry in model.values() if entry['answer'] == True]
-    false_probs = [entry['probability'] for entry in model.values() if entry['answer'] == False]
-
+    true_probs = [entry['probability'] for entry in model.values() if entry['answer']]
+    false_probs = [entry['probability'] for entry in model.values() if not entry['answer']]
 
     fig = go.Figure()
     fig.add_trace(go.Histogram(
         x = true_probs,
         yaxis = 'y2',
-        marker = dict(
-            color = 'olivedrab'
-        ),
-        xbins=dict(start=0.00, end=1.00, size=0.01)
+        marker = {
+            "color": 'olivedrab'
+        },
+        xbins={"start": 0.00, "end": 1.00, "size": 0.01}
     ))
 
     fig.add_trace(go.Histogram(
-    x = false_probs,
-    yaxis = 'y2',
-    marker = dict(
-        color = 'darkred'
-    ),
-    xbins=dict(start=0.00, end=1.00, size=0.01)
+        x = false_probs,
+        yaxis = 'y2',
+        marker = {
+            "color": 'darkred'
+        },
+        xbins={"start": 0.00, "end": 1.00, "size": 0.01}
     ))
 
-    fig.add_shape(type='line',
-              x0= slith, x1=slith, y0=0, y1=1,
-              xref='x', yref='paper',
-              line=dict(color='Red', width=2, dash='dash'))
+    fig.add_shape(type='line', x0= slith, x1=slith, y0=0, y1=1, xref='x', yref='paper',
+                  line={"color": 'Red', "width": 2, "dash": "dash"})
 
     fig.update_layout(
-    barmode='stack',
-    xaxis_title='Probabilities',
-    yaxis_title='Frequency',
-    title='Stacked Histogram by Probabilities')
+        barmode='stack',
+        xaxis_title='Probabilities',
+        yaxis_title='Frequency',
+        title='Stacked Histogram by Probabilities'
+    )
 
     return fig
 
@@ -209,11 +189,11 @@ def plot_init_prc(prc_data_ndarray, slith: float):
     if slith is not None:
         idx = (np.abs(threshold - slith)).argmin()
         threshold_trace = go.Scatter(
-        x=[recall[idx]],
-        y=[precision[idx]],
-        mode='markers',
-        marker=dict(color='red', size=10),
-        name=f'Threshold = {threshold[idx]:.2f}'
+            x=[recall[idx]],
+            y=[precision[idx]],
+            mode='markers',
+            marker={"color": 'red', "size": 10},
+            name=f'Threshold = {threshold[idx]:.2f}'
         )
         fig.add_trace(threshold_trace)
         fig.add_annotation(
@@ -225,14 +205,14 @@ def plot_init_prc(prc_data_ndarray, slith: float):
         )
 
     fig.update_layout(
-    title='Precision-Recall Curve',
-    xaxis_title='Recall',
-    yaxis_title='Precision',
-    legend_title='Models',
-    template='plotly_white',
-    showlegend = True,
-    xaxis=dict(range=[0.8, 1.05]),
-    yaxis=dict(range=[0.8, 1.05]),
+        title='Precision-Recall Curve',
+        xaxis_title='Recall',
+        yaxis_title='Precision',
+        legend_title='Models',
+        template='plotly_white',
+        showlegend = True,
+        xaxis={"range": [0.8, 1.05]},
+        yaxis={"range": [0.8, 1.05]},
     )
 
     return fig
@@ -246,11 +226,11 @@ def update_prc(org_fig, prc_data_ndarray, slith: float):
     if slith is not None:
         idx = (np.abs(threshold - slith)).argmin()
         threshold_trace = go.Scatter(
-        x=[recall[idx]],
-        y=[precision[idx]],
-        mode='markers',
-        marker=dict(color='red', size=10),
-        name=f'Threshold = {threshold[idx]:.2f}'
+            x=[recall[idx]],
+            y=[precision[idx]],
+            mode='markers',
+            marker={"color": 'red', "size": 10},
+            name=f'Threshold = {threshold[idx]:.2f}'
         )
         fig.add_trace(threshold_trace)
 
@@ -269,16 +249,16 @@ def plot_init_roc(roc_data_ndarray, slith: float):
     fpr, tpr, threshold = roc_data_ndarray
     fig = go.Figure()
     fig.add_trace(go.Scatter(x= fpr, y= tpr, mode='lines', name='Model 1'))
-    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='Random Classifier', line=dict(dash='dash')))
+    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='Random Classifier', line={"dash": 'dash'}))
 
     if slith is not None:
         idx = (np.abs(threshold - slith)).argmin()
         threshold_trace = go.Scatter(
-        x=[fpr[idx]],
-        y=[tpr[idx]],
-        mode='markers',
-        marker=dict(color='red', size=10),
-        name=f'Threshold = {threshold[idx]:.2f}'
+            x=[fpr[idx]],
+            y=[tpr[idx]],
+            mode='markers',
+            marker={"color": 'red', "size": 10},
+            name=f'Threshold = {threshold[idx]:.2f}'
         )
         fig.add_trace(threshold_trace)
 
@@ -290,16 +270,15 @@ def plot_init_roc(roc_data_ndarray, slith: float):
             arrowhead = 2
         )
 
-
-    # Update layoutff
+    # Update layout
     fig.update_layout(
         title='ROC Curve',
         xaxis_title='1-Specificity',
         yaxis_title='Sensitivity',
         legend_title='Models',
         template='plotly_white',
-        xaxis=dict(range=[-0.05, 0.25]),
-        yaxis=dict(range=[0.8, 1.05]),
+        xaxis={"range": [-0.05, 0.25]},
+        yaxis={"range": [0.8, 1.05]},
     )
 
     return fig
@@ -311,11 +290,11 @@ def update_roc(org_fig, roc_data_ndarray, slith: float):
     if slith is not None:
         idx = (np.abs(threshold - slith)).argmin()
         threshold_trace = go.Scatter(
-        x=[fpr[idx]],
-        y=[tpr[idx]],
-        mode='markers',
-        marker=dict(color='red', size=10),
-        name=f'Threshold = {threshold[idx]:.2f}'
+            x=[fpr[idx]],
+            y=[tpr[idx]],
+            mode='markers',
+            marker={"color": 'red', "size": 10},
+            name=f'Threshold = {threshold[idx]:.2f}'
         )
         fig.add_trace(threshold_trace)
         fig.add_annotation(
@@ -325,8 +304,6 @@ def update_roc(org_fig, roc_data_ndarray, slith: float):
             showarrow = True,
             arrowhead = 2
         )
-
-
     return fig
 
 def app() -> None:
@@ -335,16 +312,13 @@ def app() -> None:
     st.caption("Pages that contain three dashboards, 2D model comparison chart")
 
     st.header("Import Models with Database Path")
-    dir_model_1 = st.text_input('Model 1', value='/mnt/fs0/minye/falsefilter_api_study/db_files/N3_M2-6_20240923_000532_m0.0.1.db', help='Your First Model Database Directory.')
+    dir_model_1 = st.text_input('Model 1', value='/mnt/fs0/minye/falsefilter_api_study/db_files/N3_M2-6_20240923_000532_m0.0.1.db', help='Model Database Directory.')
     dir_model_2 = st.text_input('Model 2 (optional)', value='', help='Your Second Model Database Directory.')
-
-
 
     slider_threshold_1 = st.slider("Select confidence threshold for model 1:", 0.0, 1.0, 0.5)
     st.caption(f"Probabilities above :blue[{slider_threshold_1}] in Model 1 will be considered defects.")
     slider_threshold_2 = st.slider("Select confidence threshold for model 2:", 0.0, 1.0, 0.5)
     st.caption(f"Probabilities above :blue[{slider_threshold_2}] in Model 2 will be considered defects.")
-
 
     if st.button("Visualize model", type = 'primary'):
         if dir_model_1 and dir_model_2:
