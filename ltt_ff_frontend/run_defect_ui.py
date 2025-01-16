149d436c1d6fc9898168597250c63cce7dea813e
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-01-16 14:43:08 +0800

    fix: remove dead pages

--- ltt_ff_frontend/run_defect_ui.py
+++ ltt_ff_frontend/run_defect_ui.py
@@ -8,14 +8,12 @@ if __name__ == "__main__":
     logger.debug("Loading main UI ...")
 
     result_viewer = st.Page(result_viewer.app, url_path="result_viewer",
-                               title="Result Viewer", icon=":material/search_check_2:")
+                            title="Result Viewer", icon=":material/search_check_2:")
     inference_api = st.Page(inference_api.app, url_path="inference_api",
                             title="Inference", icon=":material/search_check_2:")
     finetuning_api = st.Page(finetuning_api.app, url_path="training_api", title="Fine-tuning", icon=":material/build:")
-    defect_viz = st.Page(defect_viz.app, url_path="defect_viz",
-                         title="Defect visualization", icon=":material/browse_activity:")
 
-    pg = st.navigation([result_viewer, inference_api, finetuning_api, defect_viz])
+    pg = st.navigation([result_viewer, inference_api, finetuning_api])
 
     st.set_page_config(page_title="Lasertec Defect Filter UI",
                        page_icon=":material/manufacturing:",
