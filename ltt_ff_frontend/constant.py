668b10a1b294c73f81b33a1b2eb9d3cc542488d2
mylimll <lim.min.ye@lasertectaiwan.com>
2025-02-10 17:12:18 +0800

    feat: update allowed lrf list

--- ltt_ff_frontend/constant.py
+++ ltt_ff_frontend/constant.py
@@ -2,6 +2,6 @@ API_ROOT = "http://localhost:6500/api/v1/"
 
 DEFAULT_THRESHOLD = 0.174
 
-ALLOWED_LRF_TYPES = ['_ADD', '_ADC', '_classified', '_filtered']
+ALLOWED_LRF_TYPES = ['_ADD', '_ADC', '_classified', '_Classified', '_filtered']
 
 TIMEOUT = 10
