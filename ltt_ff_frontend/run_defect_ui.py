ab86c07f7d6afaacbba0d57dad1784d866a7cbe3
mylimll <lim.min.ye@lasertectaiwan.com>
2025-03-14 17:57:26 +0800

    feat: update sidebar version

--- ltt_ff_frontend/run_defect_ui.py
+++ ltt_ff_frontend/run_defect_ui.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
                        page_icon=":material/manufacturing:",
                        layout="wide", initial_sidebar_state="collapsed")
 
-    st.sidebar.markdown("###### FF-FE v0.4.0")
+    st.sidebar.markdown("###### FF-FE v0.5.0")
 
     pg.run()
