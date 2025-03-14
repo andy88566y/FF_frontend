266c57c836fc4fcb8a95405e28b7b4bd55bf5578
mylimll <lim.min.ye@lasertectaiwan.com>
2025-03-14 17:05:49 +0800

    feat: updated finetune params constant

--- ltt_ff_frontend/constant.py
+++ ltt_ff_frontend/constant.py
@@ -1,22 +1,23 @@
 API_ROOT = "http://localhost:6500/api/v1/"
 TIMEOUT = 10
+
 OPTIMIZER_TYPE = ["Adam", "AdamW"]
 OPTIMIZER_PARAMS = {
     "Adam": {
-        "weight_decay":  (0.0, "%0.2f", 0.0, 1.0), # (default_value, accuracy, min_value, max_value)
+        "weight_decay":  (0.0, "%0.3f", 0.0, 1.0), # (default_value, accuracy, min_value, max_value)
     },
     "AdamW": {
-        "weight_decay": (0.01, "%0.2f", 0.0, 1.0),
+        "weight_decay": (0.01, "%0.3f", 0.0, 1.0),
     },
 }
 LOSS_TYPE = ["bce", "focal"]
 LOSS_PARAMS = {
     "bce": {
-        "pos_weight": (None, "%0.2f", 0.0, 1.0),
+        "pos_weight": (None, "%d", 0, 100),
     },
     "focal": {
         "alpha": (0.25, "%0.2f", 0.0, 1.0),
-        "gamma": (2.0, "%0.2f", 0.0, 5.0),
+        "gamma": (2.0, "%0.2f", 0.0, 10.0),
     },
 }
 LR_SCHEDULER_TYPE = ["disable", "plateau"]
@@ -25,10 +26,10 @@ LR_SCHEDULER_PARAMS = {
 
     },
     "plateau": {
-        "factor": (0.1, "%0.1f", 0.0, 1.0),
-        "patience": (5, "%d", 0, 10),
-        "threshold": (1e-4, "%0.4f", 0.0, 1.0),
-        "min_lr": (1e-6, "%0.6f", 0.0, 1.0),
-        "eps": (1e-6, "%0.6f", 0.0, 1.0),
+        "factor": (0.1, "%0.2f", 0.0, 1.0),
+        "patience": (5, "%d", 0, 1000),
+        "threshold": (1e-4, "%0.6f", 0.0, 1.0),
+        "min_lr": (1e-6, "%0.8f", 0.0, 1.0),
+        "eps": (1e-6, "%0.8f", 0.0, 1.0),
     },
 }
