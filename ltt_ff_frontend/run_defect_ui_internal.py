f4e3fb4f417462e63ea10b76e46c8fdfb57bdad0
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-04-08 13:42:52 +0800

    build: update version

--- ltt_ff_frontend/run_defect_ui_internal.py
+++ ltt_ff_frontend/run_defect_ui_internal.py
@@ -80,6 +80,6 @@ if __name__ == "__main__":
         initial_sidebar_state="collapsed",
     )
 
-    st.sidebar.markdown("###### FF-FE v0.5.0 IH")
+    st.sidebar.markdown("###### FF-FE v0.6.0 IH")
 
     pg.run()
