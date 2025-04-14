51eab0d36d69ee5f0685f8cb46f89e0c5ea43455
mylimll <lim.min.ye@lasertectaiwan.com>
2025-04-14 11:32:07 +0800

    feat: combine finetune & basetrain UI
    - Also added normal inference page back to internal build for easier debugging

--- ltt_ff_frontend/run_defect_ui_internal.py
+++ ltt_ff_frontend/run_defect_ui_internal.py
@@ -3,8 +3,8 @@ from loguru import logger
 
 from ltt_ff_frontend.defect_review_ui import defect_review_gui
 from ltt_ff_frontend.defect_ui import (
-    basetrain_api,
     finetuning_api,
+    inference_api,
     multilot_inference_recipe_api,
 )
 from ltt_ff_frontend.result_viewer import result_viewer, result_viewer_recipe
@@ -25,6 +25,9 @@ if __name__ == "__main__":
         title="Result Viewer Recipe",
         icon=":material/search_check_2:",
     )
+    page_inference_api = st.Page(
+        inference_api.app, url_path="inference_api", title="Inference", icon=":material/content_paste_search:"
+    )
     page_multilot_inference_recipe_api = st.Page(
         multilot_inference_recipe_api.app,
         url_path="multilot_inference_recipe_api",
@@ -37,18 +40,15 @@ if __name__ == "__main__":
     page_finetuning_api = st.Page(
         finetuning_api.app, url_path="training_api", title="Fine-tuning", icon=":material/build:"
     )
-    page_basetrain_api = st.Page(
-        basetrain_api.app, url_path="basetrain_api", title="Base-training", icon=":material/build:"
-    )
 
     pg = st.navigation(
         [
             page_result_viewer,
             page_result_viewer_recipe,
+            page_inference_api,
             page_multilot_inference_recipe_api,
             page_defect_review_gui,
             page_finetuning_api,
-            page_basetrain_api,
         ]
     )
 
