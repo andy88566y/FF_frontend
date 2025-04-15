1a981fac198ed9640aee698f28e21a09a9a8d8fa
Ron Yang <ron.yang@lasertectaiwan.com>
2025-04-15 12:19:22 +0800

    feat: add shared component example

--- ltt_ff_frontend/comparison_ui/comparison_viewer.py
+++ ltt_ff_frontend/comparison_ui/comparison_viewer.py
@@ -1,5 +1,6 @@
 import streamlit as st
+import shared_components.venn_diagram
 
 def app() -> None:
     st.title("Comparison Viewer")
-    
+    st.pyplot(gen_venn_diagram())
---
+++ ltt_ff_frontend/shared_components/venn_diagram.py
@@ -0,0 +1,15 @@
+import plotly.graph_objects as go
+
+##################################################################
+#                                                                #
+# Shared Components                                              #
+#                                                                #
+# This file stores components or charts                          #
+# That will be used in multiple viewers or components.           #
+#                                                                #
+##################################################################
+
+def gen_venn_diagram() -> go.Figure:
+    fig = go.Figure()
+
+    return fig
