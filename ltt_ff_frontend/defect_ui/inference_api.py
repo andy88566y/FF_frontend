33f78f2f15d2516d86a70df2094388abb0119d7a
mylimll <lim.min.ye@lasertectaiwan.com>
2025-05-07 18:22:54 +0800

    fix: inference UI status check fix

--- ltt_ff_frontend/defect_ui/inference_api.py
+++ ltt_ff_frontend/defect_ui/inference_api.py
@@ -98,12 +98,12 @@ def app() -> None:
 
         # Validate valid yaml config
         recipe_validity = api_helper.is_valid_yaml_config(recipe, "recipe")
-        if recipe_validity["status"] == "completed":
+        if recipe_validity["status"] != "completed":
             st.error(recipe_validity["message"])
             logger.error(recipe_validity["message"])
             return
         lot_info_validity = api_helper.is_valid_yaml_config(inf_config, "lots")
-        if lot_info_validity["status"] == "completed":
+        if lot_info_validity["status"] != "completed":
             st.error(lot_info_validity["message"])
             logger.error(lot_info_validity["message"])
             return
