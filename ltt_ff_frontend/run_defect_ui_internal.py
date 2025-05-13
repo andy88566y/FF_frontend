2f8aa9eec54e630ff2f153d9a109165a9894f163
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-05-13 16:56:37 +0800

    build: bump to 0.8.0

--- ltt_ff_frontend/run_defect_ui_internal.py
+++ ltt_ff_frontend/run_defect_ui_internal.py
@@ -60,6 +60,6 @@ if __name__ == "__main__":
         initial_sidebar_state="collapsed",
     )
 
-    st.sidebar.markdown("###### FF-FE v0.7.0 IH")
+    st.sidebar.markdown("###### FF-FE v0.8.0 IH")
 
     pg.run()
