57aacf053f2f138a192f91db1c19ef5bc01af17f
Michael Hsieh <michael.hsieh@lasertectaiwan.com>
2025-06-10 22:14:34 +0800

    feat: allow thc thd to be visible and switch stats table to be lower

--- ltt_ff_frontend/result_viewer/result_viewer.py
+++ ltt_ff_frontend/result_viewer/result_viewer.py
@@ -136,11 +136,6 @@ def app() -> None:
     with st.container():
         st.subheader("Inference Results")
 
-    with st.container():
-        selected_lot_id_list = multi_lot_stats.draw_stats_df(
-            multi_lot_model_data, recipe, inference_result_dir, key="recipe_stats_df"
-        )
-
     with st.expander(label="OOS Summary"):
         oos_summary_component.gen(recipe, inference_result_dir)
 
@@ -154,6 +149,11 @@ def app() -> None:
     with st.container():
         with st.expander(label="LRF ClassType count"):
             class_type_component.gen(inference_result_dir, multi_lot_model_data.model_metadata_list)
+
+    with st.container():
+        selected_lot_id_list = multi_lot_stats.draw_stats_df(
+            multi_lot_model_data, recipe, inference_result_dir, key="recipe_stats_df"
+        )
 
     if len(recipe["recipes"]) == 1:
         # Columns for drawing distribution chart and ROC curve
