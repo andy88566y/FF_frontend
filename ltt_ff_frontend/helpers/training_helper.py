cc149aaf4afb4d870f787526bbc1114e8519db70
mylimll <lim.min.ye@lasertectaiwan.com>
2025-06-17 14:43:33 +0800

    feat: block whitespace from model naming

--- ltt_ff_frontend/helpers/training_helper.py
+++ ltt_ff_frontend/helpers/training_helper.py
@@ -3,7 +3,7 @@ import re
 
 def get_output_model_name(site: str, tool: str, tech_layer: str, layer_group: str) -> str:
     # Assert none of the model name components contain symbols
-    assert re.fullmatch(r"[A-Za-z0-9 ]*", f"{site}{tool}{tech_layer}{layer_group}") is not None, (
+    assert re.fullmatch(r"[A-Za-z0-9]*", f"{site}{tool}{tech_layer}{layer_group}") is not None, (
         "Site/Tool/TechLayer/LayerGroup cannot contain symbols!"
     )
     return "#".join([site, tool, tech_layer, layer_group])
