c80168f0027d197b0b4d4ca5878b9d569dedfb3d
mylimll <lim.min.ye@lasertectaiwan.com>
2025-06-17 10:06:17 +0800

    feat: moved inf result table into expander

--- ltt_ff_frontend/result_viewer/result_viewer.py
+++ ltt_ff_frontend/result_viewer/result_viewer.py
@@ -146,11 +146,10 @@ def app() -> None:
         particle_mode_defects_component.gen(inference_result_dir)
 
     # TODO: Get classtype grouping from backend
-    with st.container():
-        with st.expander(label="LRF ClassType count"):
-            class_type_component.gen(inference_result_dir, multi_lot_model_data.model_metadata_list)
+    with st.expander(label="LRF ClassType count"):
+        class_type_component.gen(inference_result_dir, multi_lot_model_data.model_metadata_list)
 
-    with st.container():
+    with st.expander(label="Inference Result Table"):
         selected_lot_id_list = multi_lot_stats.draw_stats_df(
             multi_lot_model_data, recipe, inference_result_dir, key="recipe_stats_df"
         )
