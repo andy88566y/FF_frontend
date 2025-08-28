1a78c59fdee1ea36c47af1be56bf3aa55681b5f4
Peter Lin <peterlinth@gpu01.local.local>
2025-08-28 13:56:05 +0800

    restore file

--- ltt_ff_frontend/helpers/api_helper.py
+++ ltt_ff_frontend/helpers/api_helper.py
@@ -1567,6 +1567,8 @@ def generate_diff_images(data_yaml_path: str, lot_id: str, defect_id: str, norm:
 #####################################################################################################
 # Regression Test                                                                                #
 #####################################################################################################
+
+
 @st.cache_data(ttl="60s")
 def fetch_valid_lots(test_data: dict[str, Any], gen_stats: bool) -> dict[str, Any]:
     data_lots = {d["lot_id"]: d for d in test_data["data_paths"]}
@@ -1575,6 +1577,7 @@ def fetch_valid_lots(test_data: dict[str, Any], gen_stats: bool) -> dict[str, An
         json={"data_lots": data_lots, "gen_stats": gen_stats},
         timeout=TIMEOUT,
     )
+
     if r.json()["status"] == "completed":
         return r.json()
     else:
@@ -1590,11 +1593,13 @@ def fetch_regression_result(
         json={"test_cases": test_cases, "prefixes": prefixes, "result_dir": result_dir, "mode": mode},
         timeout=TIMEOUT,
     )
+
     if r.json()["status"] == "completed":
         return r.json()
     else:
         logger.error(f"Error occurred when calling Regression Result API: {r.json()['message']}")
         raise ValueError(f"Error occurred when calling Regression Result  API: {r.json()['message']}")
+
 
 def request_regression_test(
     output_dir_root: str,
@@ -1614,6 +1619,7 @@ def request_regression_test(
         },
         timeout=TIMEOUT,
     )
+
     if r.json()["status"] == "started":
         return r.json()
     else:
