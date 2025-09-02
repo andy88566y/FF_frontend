1698ab099b122c444b8379b171bc1d3e9bfe2254
kappa023 <kappa.lee@lasertectaiwan.com>
2025-09-02 11:23:57 +0800

    feat: add missing value in venn diagram

--- ltt_ff_frontend/shared_components/venn_diagram.py
+++ ltt_ff_frontend/shared_components/venn_diagram.py
@@ -1,4 +1,7 @@
 import streamlit as st
+
+import numpy as np
+
 from ltt_ff_frontend.shared_components.chart_drawer.venn import plot_venn
 
 
@@ -8,20 +11,21 @@ def gen(
     model_names: list[str],
     split_lot: bool,
 ) -> None:
-    defect_ids, _,  prob_lists, ans, lot_ids = aggregated_model_data
-    classifications = ["Defect" if label == 1 else "Non-defect" if label == 0 else "Unlabeled" for label in ans]
-    tp_datas, tn_datas, _, _ = intersections
+    _, _,  _, ans, _ = aggregated_model_data
+    tp_datas, tn_datas, tp_corrections, tn_corrections = intersections
     tp_sets = [set(model_correction) for model_correction in tp_datas]
     tn_sets = [set(model_correction) for model_correction in tn_datas]
+    missed_tp_count = np.sum(np.all( np.asarray(tp_corrections) == 0, axis=1))
+    missed_tn_count = np.sum(np.all( np.asarray(tn_corrections) == 0, axis=1))
 
     tp_col, tn_col = st.columns(2)
     with tp_col:
         st.plotly_chart(
-            plot_venn(sets=tp_sets, labels=model_names, title=f"True Defects Venn Diagram (Total: {ans.count(1)})"),
+            plot_venn(sets=tp_sets, labels=model_names, miss_count=missed_tp_count, title=f"True Defects Venn Diagram (Total: {ans.count(1)})"),
             clear_figure=True,
         )
     with tn_col:
         st.plotly_chart(
-            plot_venn(sets=tn_sets, labels=model_names, title=f"False Defects Venn Diagram (Total: {ans.count(0)})"),
+            plot_venn(sets=tn_sets, labels=model_names, miss_count=missed_tn_count, title=f"False Defects Venn Diagram (Total: {ans.count(0)})"),
             clear_figure=True,
         )
