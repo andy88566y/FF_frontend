c0be73719922e41ba27a1720dcbd5082d3bcbe2c
mylimll <lim.min.ye@lasertectaiwan.com>
2025-09-02 13:05:16 +0800

    fix: list to tuple

--- ltt_ff_frontend/shared_components/venn_diagram.py
+++ ltt_ff_frontend/shared_components/venn_diagram.py
@@ -1,6 +1,5 @@
-import streamlit as st
-
 import numpy as np
+import streamlit as st
 
 from ltt_ff_frontend.shared_components.chart_drawer.venn import plot_venn
 
@@ -11,21 +10,31 @@ def gen(
     model_names: list[str],
     split_lot: bool,
 ) -> None:
-    _, _,  _, ans, _ = aggregated_model_data
-    tp_datas, tn_datas, tp_corrections, tn_corrections = intersections
+    _, _, _, ans, _ = aggregated_model_data
+    tp_datas, tn_datas, tp_corrections, tn_corrections = tuple(intersections)
     tp_sets = [set(model_correction) for model_correction in tp_datas]
     tn_sets = [set(model_correction) for model_correction in tn_datas]
-    missed_tp_count = np.sum(np.all( np.asarray(tp_corrections) == 0, axis=1))
-    missed_tn_count = np.sum(np.all( np.asarray(tn_corrections) == 0, axis=1))
+    missed_tp_count = np.sum(np.all(np.asarray(tp_corrections) == 0, axis=1))
+    missed_tn_count = np.sum(np.all(np.asarray(tn_corrections) == 0, axis=1))
 
     tp_col, tn_col = st.columns(2)
     with tp_col:
         st.plotly_chart(
-            plot_venn(sets=tp_sets, labels=model_names, miss_count=missed_tp_count, title=f"True Defects Venn Diagram (Total: {ans.count(1)})"),
+            plot_venn(
+                sets=tp_sets,
+                labels=model_names,
+                miss_count=missed_tp_count,
+                title=f"True Defects Venn Diagram (Total: {ans.count(1)})",
+            ),
             clear_figure=True,
         )
     with tn_col:
         st.plotly_chart(
-            plot_venn(sets=tn_sets, labels=model_names, miss_count=missed_tn_count, title=f"False Defects Venn Diagram (Total: {ans.count(0)})"),
+            plot_venn(
+                sets=tn_sets,
+                labels=model_names,
+                miss_count=missed_tn_count,
+                title=f"False Defects Venn Diagram (Total: {ans.count(0)})",
+            ),
             clear_figure=True,
         )
